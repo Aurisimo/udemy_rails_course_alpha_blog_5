8c39cc450119445463ae3b99a049b9b016c1b813
Aurelijus Lape <lape_aurelijus@yahoo.com>
2020-10-27 00:02:53 +0200

    Add user profile view and action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    hash = Digest::MD5.hexdigest(user.email)
+
+    image_tag "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}",
+              alt: user.username, class: 'rounded mx-auto d-block'
+  end
 end
